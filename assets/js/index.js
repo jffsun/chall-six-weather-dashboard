d8bf0b1fbc1a07b8c1f04ab5841e5826b97a2bae
Jeffrey Sun <jeffreysun811@gmail.comgit configgit config --global user.name Jeffrey>
2022-08-12 15:07:34 -0700

    submit btn event handler, DS.store to gitignore, script.js added

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,31 +1,48 @@
+var recentList = $('#recent-list')
+var recents = [];
+
 // Pulling the api key from the config.js
 console.log(api.key);
 
 console.log(`https://api.openweathermap.org/data/2.5/onecall?lat=${api.lat}&lon=${api.lon}&appid=${api.key}`);
 
-// var recentList = $('recent-list')
-// var recents = [];
-
-// // Searchbar function
-// var searchFormEl = document.querySelector('#search-form');
-
-// function handleSearchFormSubmit(event) {
-//   event.preventDefault();
-
-//   var searchInputVal = document.querySelector('#search-input').value;
-//   var formatInputVal = document.querySelector('#format-input').value;
-
-//   if (!searchInputVal) {
-//     console.error('You need a search input value!');
-//     return;
-//   }
-
-//   var queryString = './search-results.html?q=' + searchInputVal + '&format=' + formatInputVal;
-
-//   location.assign(queryString);
-// }
-
-// searchFormEl.addEventListener('submit', handleSearchFormSubmit);
+function searchApi(query) {
+    var weatherQueryUrl = 'https://www.loc.gov/search/?fo=json';
+  
+    if (format) {
+      locQueryUrl = 'https://www.loc.gov/' + format + '/?fo=json';
+    }
+  
+    locQueryUrl = locQueryUrl + '&q=' + query;
+  
+    fetch(locQueryUrl)
+      .then(function (response) {
+        if (!response.ok) {
+          throw response.json();
+        }
+  
+        return response.json();
+      })
+      .then(function (locRes) {
+        // write query to page so user knows what they are viewing
+        resultTextEl.textContent = locRes.search.query;
+  
+        console.log(locRes);
+  
+        if (!locRes.results.length) {
+          console.log('No results found!');
+          resultContentEl.innerHTML = '<h3>No results found, search again!</h3>';
+        } else {
+          resultContentEl.textContent = '';
+          for (var i = 0; i < locRes.results.length; i++) {
+            printResults(locRes.results[i]);
+          }
+        }
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }
 
 // // Append unordered list items to #recent
 // function renderRecents () {
@@ -43,4 +60,4 @@ console.log(`https://api.openweathermap.org/data/2.5/onecall?lat=${api.lat}&lon=
 //     }
 // }
 
-// // Default Irvine for 5-day forecast 
+// Default Irvine for 5-day forecast 
