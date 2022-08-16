6105235eac84ecedd23e493d8240bda38290e570
Jeffrey Sun <jeffreysun811@gmail.comgit configgit config --global user.name Jeffrey>
2022-08-15 18:14:53 -0700

    display weather data, color code UV Index

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,17 +1,16 @@
 var resultTextEl = $('#result-text');
+var dateToday = moment().format('l');
 var recentList = $('#recent-list');
 var tempToday = $('#temp-today');
 var windToday = $('#wind-today');
 var humidToday = $('#humid-today');
 var uvToday = $('#uv-today');
-
-
-
+var uvColor = $('#uv-color');
 var recents = [];
 
 // Pulling the api key from the config.js
-console.log(api.key);
-console.log(`https://api.openweathermap.org/data/2.5/weather?q=${api.city}&appid=${api.key}&units=imperial`)
+// console.log(api.key);
+// console.log(`https://api.openweathermap.org/data/2.5/weather?q=${api.city}&appid=${api.key}&units=imperial`)
 
 // Searchbar function
 var searchFormEl = $('#search-form');
@@ -26,47 +25,45 @@ function handleSearchFormSubmit(e) {
     return;
   }
 
-  console.log(searchInputVal);
   var city = searchInputVal;
   searchApi(city);
 }
 
 searchFormEl.submit(handleSearchFormSubmit);
 
-// First call API for latitutude and longitude
+// Call API for city lat and lon coordinates
 function searchApi(city) {
-    var coordinateQueryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + api.key + "&units=imperial"
-    console.log(coordinateQueryUrl);
 
+    var coordinateQueryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + api.key
+    console.log(coordinateQueryUrl)
     fetch(coordinateQueryUrl)
     .then(function (response) {
       if (response.ok) {
-        console.log(response);
         response.json().then(function (geoData) {
-          console.log(geoData);
-          console.log(geoData.coord)
+
           //TO DO: display and store recent search function
+          resultTextEl.text(geoData.name + " " + dateToday);
           weatherApi(geoData);
+          forecastApi(geoData);
         });
       } else {
         alert('Error: ' + response.statusText);
       }
     })
-    // .catch(function (error) {
-    //   alert('Unable to connect to GitHub');
-    // });
+    .catch(function (e) {
+      console.log(e);
+    });
 };
 
-// Then call API for weather conditions
-function weatherApi(geoData) {
-  var weatherQueryUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + geoData.coord.lat + "&lon=" + geoData.coord.lon + "&exclude=hourly,daily&appid=" + api.key
+// Call API for current city weather conditions
+function weatherApi (geoData) {
+  var weatherQueryUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + geoData.coord.lat + "&lon=" + geoData.coord.lon + "&exclude=hourly,daily&appid=" + api.key + "&units=imperial"
 
   console.log(weatherQueryUrl);
 
   fetch(weatherQueryUrl)
   .then(function (response) {
     if (response.ok) {
-      console.log(response);
       response.json().then(function (weatherData) {
         console.log(weatherData);
         //TO DO: display and store recent search function
@@ -76,16 +73,54 @@ function weatherApi(geoData) {
       alert('Error: ' + response.statusText);
     }
   })
-  // .catch(function (error) {
-  //   alert('Unable to connect to GitHub');
-  // });
+  .catch(function (e) {
+    console.log(e);
+  });
 };
-// TO DO: trim display data to today forecast el with units
+
+// Display city weather data to today forecast
 function todayForecast (weatherData) {
-  tempToday.text(weatherData.current.temp)
-  windToday.text(weatherData.current.wind_speed)
-  humidToday.text(weatherData.current.humidity)
-  uvToday.text(weatherData.current.uvi)
+  var temp = weatherData.current.temp
+  var wind = weatherData.current.wind_speed
+  var humid = weatherData.current.humidity
+  var uvi = weatherData.current.uvi
+
+  tempToday.text(`Temp: ${temp}°F`);
+  windToday.text(`Wind: ${wind} MPH`);
+  humidToday.text(`Humidity: ${humid} %`);
+
+  // Display UVI value
+  uvColor.text(uvi);
+  
+  // Get color code value and apply color class accordingly
+  var uviTrunc = Math.trunc(uvi)
+  uvColor.attr("class", `uv${uviTrunc}`);
+  
+}
+
+// TO DO: 5-day forecast (default Irvine) 
+function forecastApi (geoData) {
+  var forecastQueryUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + geoData.coord.lat + "&lon=" + geoData.coord.lon + "&exclude=hourly,daily&appid=" + api.key + "&units=imperial"
+  console.log(forecastQueryUrl);
+
+  fetch(forecastQueryUrl)
+  .then(function (response) {
+    if (response.ok) {
+      response.json().then(function (forecastData) {
+        console.log(forecastData);
+        fiveForecast(forecastData);
+      });
+    } else {
+      alert('Error: ' + response.statusText);
+    }
+  })
+  .catch(function (e) {
+    console.log(e);
+  });
+}
+
+function fiveForecast(forecastData) {
+  
 }
 
 // TO DO: Search history - append unordered list items to #recent
@@ -104,4 +139,3 @@ function renderRecents () {
     }
 }
 
-// TO DO: 5-day forecast (default Irvine) 
