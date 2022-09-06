116de2f9adbe27bd616500d9b0140c282d66bfca
Jeffrey Sun <jeffreysun811@gmail.comgit configgit config --global user.name Jeffrey>
2022-09-05 20:01:23 -0700

    comments to config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,17 +1,10 @@
-
 // current date and time
 const d = new Date();
-console.log(d);
 
 // current date and time in ms
 dtNow = Date.now()
 
-// get specific date/time info. month is 0 indexed
-console.log(d.getMonth());
-console.log(d.getDate());
-
-// API Key can be referenced but will not be pushed 
-
+// API Key would normally not be pushed to online repo, but is being pushed for GitHub Pages functionality
 const api = {
     key: 'b503d8a6459931e887b80c46f6457b28',
     lat: '33.67',
